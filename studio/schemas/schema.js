c04285b3d5a595c5ed18ee567f10d95de82680b3
Radek Zajkowski <radek@nofixedaddressinc.com>
2020-09-10 12:06:47 -0400

    registering property page with schema

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -9,6 +9,7 @@ import author from './documents/author'
 import category from './documents/category'
 import post from './documents/post'
 import siteSettings from './documents/siteSettings'
+import propertyPage from './documents/propertyPage'
 
 // Object types
 import bodyPortableText from './objects/bodyPortableText'
